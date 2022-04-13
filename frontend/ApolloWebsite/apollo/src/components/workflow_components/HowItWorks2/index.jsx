437ea35bb5c6255eed4736819b10031dc29afff3
Naman Kapasi <kapasinh@berkeley.edu>
2022-04-12 19:20:58 -0700

    merge conflict time

--- frontend/ApolloWebsite/apollo/src/components/workflow_components/HowItWorks2/index.jsx
+++ frontend/ApolloWebsite/apollo/src/components/workflow_components/HowItWorks2/index.jsx
@@ -6,9 +6,9 @@ function HowItWorks2(props) {
   const { browseConcerts1, browseConcerts2 } = props;
 
   return (
-    <div className="how-it-works-1">
+    <div className="how-it-works-2">
       <IconWallet />
-      <div className="frame-11-1">
+      <div className="frame-11-2">
         <div className="browse-concerts-2 h4">{browseConcerts1}</div>
         <div className="browse-concerts-3 poppins-normal-steel-gray-18px">{browseConcerts2}</div>
       </div>
