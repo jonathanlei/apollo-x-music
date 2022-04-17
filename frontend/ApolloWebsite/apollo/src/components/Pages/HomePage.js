06d11165840764aaf96513a2d8078dacaf0aab68
kylung0208 <kylung2008@gmail.com>
2022-04-17 12:36:13 -0700

    add init footer.js and footer.css file

--- frontend/ApolloWebsite/apollo/src/components/Pages/HomePage.js
+++ frontend/ApolloWebsite/apollo/src/components/Pages/HomePage.js
@@ -2,13 +2,15 @@ import Nav from "../Nav";
 import Hero from "../Hero";
 import WorkFlow from "../WorkFlow";
 import EventSlider from "../EventSlider";
+import Footer from "../Footer"
 function HomePage() {
   return (
     <>
       <Hero></Hero>
       <WorkFlow></WorkFlow>
       <EventSlider></EventSlider>
-      </>
+      <Footer></Footer>
+    </>
   );
 }
 
