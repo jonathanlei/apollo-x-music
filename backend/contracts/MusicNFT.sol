d89244e69ec1d71900059dac37bc2964b8ee2a7c
Naman Kapasi <kapasinh@berkeley.edu>
2022-04-17 13:34:38 -0700

    Finished Backend Hopefully

--- backend/contracts/MusicNFT.sol
+++ backend/contracts/MusicNFT.sol
@@ -1,3 +1,5 @@
+//SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.1;
 
 
@@ -5,8 +7,10 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
+import './ERC2981ContractWideRoyalties.sol';
 
-contract MusicNFT is ERC721URIStorage{
+
+contract MusicNFT is ERC721URIStorage, ERC2981ContractWideRoyalties{
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
@@ -20,14 +24,28 @@ contract MusicNFT is ERC721URIStorage{
         console.log("This is my NFT Contract, woah");
     }
 
-    function mintTicket() public {
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721, ERC2981Base)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function setRoyalties(address recipient, uint256 value) public {
+        _setRoyalties(recipient, value);
+    }
+
+    function mintTicket(
+        address to
+    ) external {
+
         uint newID = _tokenIds.current();
         string memory finalTokenUri = "https://jsonkeeper.com/b/RUUS";
-
-
-        
-        _safeMint(msg.sender, newID);
-
+        _safeMint(to, newID);
         _setTokenURI(newID, finalTokenUri);
         _tokenIds.increment();
 
@@ -41,3 +59,8 @@ contract MusicNFT is ERC721URIStorage{
 
 
 }
+
+
+
+
+
---
+++ backend/contracts/MusicNFTFactory.sol
@@ -0,0 +1,31 @@
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./MusicNFT.sol";
+
+contract MusicNFTFactory is Ownable {
+    MusicNFT [] public instances;
+    // TODO store token ids so they can be checked for uniqueness
+
+    event InstanceCreated(address indexed _from, address indexed _instance, uint256 indexed _instanceNumber);
+
+    address factoryWalletAddress;
+
+    function get(
+    ) external {
+        // TODO make and pass a payment splitter (https://docs.openzeppelin.com/contracts/2.x/api/payment)
+
+        MusicNFT instance = new MusicNFT(
+        );
+        instances.push(instance);
+        emit InstanceCreated(msg.sender, address(instance), instances.length-1);
+    }
+
+    function setWalletAddress(address addr) public onlyOwner {
+        factoryWalletAddress = addr;
+    }
+
+}
