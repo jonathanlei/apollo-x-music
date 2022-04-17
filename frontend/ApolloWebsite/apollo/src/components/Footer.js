9bac076f801048338fa12b13550eb0220a6c8497
Naman Kapasi <kapasinh@berkeley.edu>
2022-04-17 13:55:08 -0700

    finished footer

--- frontend/ApolloWebsite/apollo/src/components/Footer.js
+++ frontend/ApolloWebsite/apollo/src/components/Footer.js
@@ -40,12 +40,14 @@ function Link(props) {
 function NewsLetter() {
     return (
         <div className="newsletter-section">
-            <h3 className="SITL-text">
-                Stay in the loop about concerts
-            </h3>
-            <div>
-                <input type="text" className="email-entry" placeholder="Enter email"></input>
-                <button>Sign Up</button>
+            <div className='newsletter-container'>
+                <h3 className="SITL-text">
+                    Stay in the loop about concerts
+                </h3>
+                <div>
+                    <input type="text" className="email-entry" placeholder="Enter email"></input>
+                    <button className="signup">Sign Up</button>
+                </div>
             </div>
         </div >
     )
